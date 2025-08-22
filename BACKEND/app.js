0adfde7a9f12bc8f84094f6b90c46b568b374107
Abhinav <abhi9651.at@outlook.com>
2025-08-22 19:25:22 +0530

    Cors updated

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -17,7 +17,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
-  "https://url-bee-frontend.vercel.app",
+  "https://url-bee-t8ji.vercel.app",
 ];
 
 app.use(
